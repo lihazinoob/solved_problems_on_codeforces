8209e8fbd435dd77160beb3c305be2aa70fcb12f
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-01-31 18:40:26 +0600

    commit

---
+++ Reset Bits.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,k;
+  cin>>n>>k;
+  for(int i = 0;i<k;i++)
+  {
+    int bit_count = floor(log(n))+1;
+    //condition if the ith bit is 1
+    if(n  )
+  }
+  return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  float bit_count = log(5);
+  cout<<bit_count;
+  return 0;
+}
